dc3ed3a67e2d12a612edf3d180649ed581dc7c17
Kade Gray <kadegray@gmail.com>
2024-08-08 13:57:26 +1200

    feat: add logout route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Register;
 use App\Livewire\Login;
 use App\Livewire\Dashboard;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
@@ -11,6 +13,12 @@ use App\Livewire\Dashboard;
 
 Route::get('/register', Register::class)->name('register');
 Route::get('/login', Login::class)->name('login');
+Route::get('/logout', function (Request $request) {
+    Auth::logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+    return redirect('login');
+})->name('logout');
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
