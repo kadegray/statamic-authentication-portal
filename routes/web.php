7b454d269b305ffa98e59beee16bc00f5eb3c2a6
Kade Gray <kadegray@gmail.com>
2024-08-08 13:48:47 +1200

    feat: add dashboard page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Register;
 use App\Livewire\Login;
+use App\Livewire\Dashboard;
 
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
@@ -10,3 +11,7 @@ use App\Livewire\Login;
 
 Route::get('/register', Register::class)->name('register');
 Route::get('/login', Login::class)->name('login');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
+});
